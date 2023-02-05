0af002a063fb45b8067ab46270f97e12838b3c27
José M. Simão <jose.simao@isel.pt>
2023-02-05 18:35:49 +0000

    adds jwt verification to ToDoController

--- phase-1/src/main/java/todolist/controller/ToDoListController.java
+++ phase-1/src/main/java/todolist/controller/ToDoListController.java
@@ -1,12 +1,15 @@
 package todolist.controller;
 // ToDoListController.java
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 import todolist.service.ToDoListItem;
 import todolist.service.ToDoListService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import todolist.service.ToDoUserService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,22 +20,27 @@ import java.util.Optional;
 public class ToDoListController {
     private static final Logger logger = LoggerFactory.getLogger(ToDoListController.class);
     private final ToDoListService toDoListService;
-
+    private final ToDoUserService userService;
     @Autowired
-    public ToDoListController(ToDoListService toDoListService) {
+    public ToDoListController(ToDoListService toDoListService, ToDoUserService userService) {
         this.toDoListService = toDoListService;
+        this.userService = userService;
     }
 
     @PostMapping()
-    public ToDoListItem createToDoListItem(@RequestBody CreateToDoListItemRequest request) {
+    public ToDoListItem createToDoListItem(@RequestBody CreateToDoListItemRequest request,
+                                           @RequestHeader("Authorization") String authorization) {
         logger.info("Create todo list item");
+        validateToken(authorization);
         ToDoListItem item = toDoListService.createToDoListItem(request.getUsername(), request.getDescription());
         return item;
     }
 
     @DeleteMapping("/{itemId}")
-    public ToDoListItem deleteToDoListItem(@PathVariable("itemId") int itemId) {
+    public ToDoListItem deleteToDoListItem(@PathVariable("itemId") int itemId,
+                                           @RequestHeader("Authorization") String authorization) {
         logger.info("Delete todo list item");
+        validateToken(authorization);
         Optional<ToDoListItem> item = toDoListService.deleteToDoListItem(itemId);
         if (!item.isEmpty()) {
             return item.get();
@@ -40,15 +48,26 @@ public class ToDoListController {
         throw new ResourceNotFoundException();
     }
 
+
+
     @GetMapping("/{username}")
-    public List<ToDoListItem> getAllItemsByUser(@PathVariable("username") String username) {
+    public List<ToDoListItem> getAllItemsByUser(@PathVariable("username") String username,
+                                                @RequestHeader("Authorization") String authorization) {
         logger.info("Get all items from user");
+        validateToken(authorization);
         Optional<List<ToDoListItem>> list = toDoListService.getToDoListItemList(username);
         if (!list.isEmpty())
             return list.get();
         return new ArrayList<>();
     }
 
-    //...
+    private void validateToken(String authorization) {
+        if (!authorization.startsWith("Bearer"))
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid JWT token");
+        String token = authorization.substring("Bearer ".length());
+        if (!userService.validateToken(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid JWT token");
+        }
+    }
 }
 
