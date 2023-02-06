b05d3998493161c48668f1b47ce7b0980ed66d8c
José M. Simão <jose.simao@isel.pt>
2023-02-06 22:26:30 +0000

    notification service has strategies

--- phase-1/src/main/java/todolist/service/NotificationService.java
+++ phase-1/src/main/java/todolist/service/NotificationService.java
@@ -4,14 +4,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class NotificationService {
     private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
+
+    List<INotificationStrategy> notificationStrategyList = new ArrayList<>();
+
+    public void addNotificationStrategy(INotificationStrategy strategy) {
+        notificationStrategyList.add(strategy);
+    }
+
     public void sendItemCreatedNotification(ToDoListItem item) {
         logger.info("Item created: " + item);
+        for(INotificationStrategy s : notificationStrategyList) {
+            s.sendCreateNotification(item);
+        }
     }
 
     public void sendItemDeletedNotification(ToDoListItem item) {
         logger.info("Item deleted: " + item);
+        for(INotificationStrategy s : notificationStrategyList) {
+            s.sendDeleteNotification(item);
+        }
     }
 }
