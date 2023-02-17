410fe0aab7205dd17936c471d6ab9b128d265e8b
José M. Simão <jsimao@cc.isel.ipl.pt>
2023-02-17 14:18:34 +0000

    removes id from todo list item

--- phase-1/src/main/java/todolist/model/ToDoListItem.java
+++ phase-1/src/main/java/todolist/model/ToDoListItem.java
@@ -1,14 +1,13 @@
 package todolist.model;
 
 public class ToDoListItem {
-    private long id;
+//    private long id;
     private String description;
     private String username;
 
     public ToDoListItem() {}
 
     public ToDoListItem(long id, String username, String description) {
-        this.id = id;
         this.username = username;
         this.description = description;
     }
@@ -18,24 +17,24 @@ public class ToDoListItem {
         this.description = description;
     }
 
-    public void setId(Long id) {
+/*    public void setId(Long id) {
         this.id = id;
-    }
+    }*/
     public String getUsername() {
         return username;
     }
     public String getDescription() {
         return description;
     }
-    public long getId() {
+/*    public long getId() {
         return id;
-    }
+    }*/
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ToDoListItem item = (ToDoListItem) o;
-        return id == item.id && username.equals(item.username) && description.equals(item.description);
+        return username.equals(item.username) && description.equals(item.description);
     }
     public String toString() {
         return "user: " + username + " / ToDo: " + description;
