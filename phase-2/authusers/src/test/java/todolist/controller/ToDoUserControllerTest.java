21b9c420afa6bf3c1fd5e37e502aea221fd86f88
José M. Simão <jsimao@cc.isel.ipl.pt>
2023-02-27 20:43:23 +0000

    user controller unit test

--- phase-2/authusers/src/test/java/todolist/controller/ToDoUserControllerTest.java
+++ phase-2/authusers/src/test/java/todolist/controller/ToDoUserControllerTest.java
@@ -9,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,6 +21,7 @@ import todolist.service.ToDoUserService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("nodb")
 public class ToDoUserControllerTest {
 
     @Autowired
